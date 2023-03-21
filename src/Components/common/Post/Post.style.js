dda3284f7ebc99c5f26e255dc49149013268a4d3
Ajay Rathod <ajay.rathod@openxcell.com>
2023-03-21 16:28:32 +0530

    VIDEO AUTOPLAY ISSUE RESOLVED

--- src/Components/common/Post/Post.style.js
+++ src/Components/common/Post/Post.style.js
@@ -1,6 +1,6 @@
 import { Paper } from '@mui/material';
 import styled from 'styled-components';
-import { COLORS, FONTS, responsive } from 'Styles/Constants';
+import { FONTS, responsive } from 'Styles/Constants';
 
 export const PostWrapper = styled(Paper)`
     padding: 10px;
@@ -47,65 +47,23 @@ export const PostWrapper = styled(Paper)`
                 padding: 0 10px 0 0;
             }
 
-            .background-img-div,
-            .background-video-div {
+            .background-img-div {
                 height: inherit;
             }
 
             .video-player {
-                height: inherit;
+                height: 350px;
                 position: relative;
+                width: 100%;
+
+                ${responsive.PHABLET`
+                    height: 300px;
+                `}
+
+                ${responsive.MOBILE`
+                    height: 250px;
+                `}
             }
         }
-    }
-`;
-
-export const VideoPlayerControlsWrapper = styled.div`
-    position: absolute;
-    bottom: 0;
-    height: 40%;
-    width: 100%;
-    background: linear-gradient(
-        0deg,
-        rgba(0, 0, 0, 0.8267682072829132) 2%,
-        rgba(182, 182, 182, 0) 72%
-    );
-    justify-content: end;
-
-    .icon-style {
-        color: ${COLORS.WHITE};
-        /* border: 1px solid red; */
-    }
-
-    .player-controls {
-        /* padding: 0; */
-
-        .pause-play {
-            gap: 5px;
-
-            .video-time {
-                color: ${COLORS.WHITE};
-            }
-        }
-    }
-
-    .progress-bar {
-        width: inherit;
-        padding: 0 15px;
-        color: ${COLORS.WHITE};
-
-        .slider-root{
-            color: ${COLORS.GREY_TEXT_COLOR};
-            
-            .slider-thumb{
-                display: none;
-            }
-    
-            .slider-track{
-                color: ${COLORS.LIGHT_GREY};
-                transition: all .3s ease-in-out;
-            }
-        }
-
     }
 `;
--- src/Components/common/Post/index.jsx
+++ src/Components/common/Post/index.jsx
@@ -1,11 +1,10 @@
 //CORE
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { Avatar, Box, Divider, IconButton, Menu, MenuItem, Typography } from '@mui/material';
+import { Avatar, Box, CardMedia, Divider, IconButton, Menu, MenuItem, Typography } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import Slider from 'react-slick';
 import moment from 'moment';
 import { useNavigate } from 'react-router-dom';
-import ReactPlayer from 'react-player';
 import OutsideClickHandler from 'react-outside-click-handler';
 
 //ICON
@@ -16,8 +15,6 @@ import DeleteIcon from '@mui/icons-material/Delete';
 //CUSTOM
 import {
     CreateUserName,
-    getTotalTimeInMins,
-    getWindowDimensions,
     stringAvatar
 } from 'Helpers/Utils';
 import { PostWrapper } from './Post.style';
@@ -26,8 +23,6 @@ import { API_URL, URL_FRIEND_PROFILE_PAGE } from 'Helpers/Paths';
 import { showToast } from 'Redux/App/Actions';
 import { ReadMore } from '../ReadMore';
 import AddPost from '../AddPost';
-import { BREAKPOINTS_VALUE } from 'Styles/Constants';
-import VideoControls from './VideoControls';
 
 const SETTINGS = {
     arrows: false,
@@ -52,19 +47,10 @@ const Post = ({
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const videoRef = useRef(null);
-    const playerRef = useRef(null);
 
     const [deleteMenu, setDeleteMenu] = useState(null);
     const [addPostDialog, setAddPostDialog] = useState(false);
     const removePopId = deleteMenu ? 'simple-popover' : undefined;
-    const [videoPlay, setVideoPlay] = useState(false);
-    const [videoPlayerHeight, setVideoPlayerHeight] = useState(350);
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-    const [videoLengthInSeconds, setVideoLengthInSeconds] = useState(0);
-    const [elapsedTimeInSeconds, setElapsedTimeInSeconds] = useState(0);
-    const [duration, setDuration] = useState('00:00');
-    const [elapsedTime, setElapsedTime] = useState('00:00');
 
     const handleEditPost = () => {
         setAddPostDialog(true);
@@ -93,8 +79,10 @@ const Post = ({
 
     const handleViewChange = useCallback((entries) => {
         for (let entry of entries) {
-            if (entry.intersectionRatio < 0.5) {
-                setVideoPlay(false);
+            if (entry.intersectionRatio > 0.9) {
+                videoRef?.current && videoRef?.current.play();
+            } else {
+                videoRef?.current && videoRef?.current.pause();
             }
         }
     }, []);
@@ -103,7 +91,7 @@ const Post = ({
         const observer = new IntersectionObserver(handleViewChange, {
             root: null,
             rootMargin: '0px',
-            threshold: 0.5
+            threshold: 0.9
         });
 
         if (videoRef.current) {
@@ -116,46 +104,6 @@ const Post = ({
             }
         };
     }, [handleViewChange]);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowDimensions(getWindowDimensions());
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    useEffect(() => {
-        if (windowDimensions?.width) {
-            if (windowDimensions?.width > BREAKPOINTS_VALUE.PHABLET) {
-                setVideoPlayerHeight(350);
-            } else if (windowDimensions?.width > BREAKPOINTS_VALUE.MOBILE) {
-                setVideoPlayerHeight(300);
-            } else {
-                setVideoPlayerHeight(250);
-            }
-        }
-    }, [windowDimensions?.width]);
-
-    useEffect(() => {
-        if (videoPlay) {
-            var interval = setInterval(() => {
-                let time = getTotalTimeInMins(playerRef.current.getCurrentTime());
-                setElapsedTimeInSeconds(playerRef.current.getCurrentTime());
-                setElapsedTime(time);
-            }, 1000);
-            return;
-        }
-
-        clearInterval(interval);
-
-        return () => {
-            setVideoPlay(false);
-            clearInterval(interval);
-        };
-    }, [videoPlay]);
 
     return (
         <PostWrapper className="flex f-column" classes={{ root: 'post-paper' }}>
@@ -208,34 +156,16 @@ const Post = ({
                                 ) : (
                                     <OutsideClickHandler
                                         onOutsideClick={() => {
-                                            setVideoPlay(false);
+                                            videoRef?.current.pause();
                                         }}>
-                                        <Box ref={videoRef} className="video-player">
-                                            <ReactPlayer
-                                                ref={playerRef}
-                                                playing={videoPlay}
-                                                url={item.mediaPath}
-                                                width="100%"
-                                                height={`${videoPlayerHeight}px`}
-                                                onPlay={() => setVideoPlay(true)}
-                                                onPause={() => setVideoPlay(false)}
-                                                onDuration={(duration) => {
-                                                    setDuration(getTotalTimeInMins(duration));
-                                                    setVideoLengthInSeconds(playerRef?.current?.getDuration());
-                                                }}
-                                                controls={false}
-                                            />
-                                            <VideoControls
-                                                videoPlay={videoPlay}
-                                                ref={playerRef}
-                                                setVideoPlay={setVideoPlay}
-                                                duration={duration}
-                                                elapsedTime={elapsedTime}
-                                                videoLengthInSeconds={videoLengthInSeconds}
-                                                elapsedTimeInSeconds={elapsedTimeInSeconds}
-                                                setElapsedTimeInSeconds={setElapsedTimeInSeconds}
-                                            />
-                                        </Box>
+                                        <CardMedia
+                                            component={'video'}
+                                            ref={videoRef}
+                                            src={item.mediaPath}
+                                            controls
+                                            className="video-player"
+                                            muted
+                                        />
                                     </OutsideClickHandler>
                                 )}
                             </Box>
